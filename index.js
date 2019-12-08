e0fa1c884709ee24ef34cf529e92fb72f17e0326
AISHA-PRO <55359117+AISHA-PRO@users.noreply.github.com>
2019-12-08 23:00:46 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+class Cat {
+  //...
+  consrtuctor(name,sex){
+    this.name = name;
+    this.sex = sex;
+  }
+  let speak=function(){
+    return `${name} says meow!`;
+  }
+}
+ 
+class Dog {
+  //...
+   consrtuctor(name,sex){
+    this.name = name;
+    this.sex = sex;
+  }  
+    let speak=function(){
+    return `${name} says woof!`;
+  }
+}
+ 
+class Bird {
+  //...
+   consrtuctor(name,sex){
+    this.name = name;
+    this.sex = sex;
+  }
+  let speak=function(){
+    if(this.sex=='female')
+      return `${name} says squawk!`;
+    else if (this.sex=='male')
+    {
+      return  "It's me! " +this.name + ", the parrot!";
+    }
+  }
+}
